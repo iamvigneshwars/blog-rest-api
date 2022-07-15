e95fa990ce638275eff96256219230dbcd495370
iamvigneshwars <iamvigneshwars@gmail.com>
2022-07-15 17:36:06 +0100

    delete post

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from aiohttp import Payload
 from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
+
+from regex import F
 
 
 app = FastAPI()
@@ -55,4 +56,18 @@ def get_post(id: int, response: Response):
     # return {"post_detail" : f"here is post {id}"}
     return {"post" : in_post}
 
+def find_index_post(id):
+    for i , p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
+@app.delete("/posts/{id}", status_code = status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    # find the index in the array that has the required ID
+    index = find_index_post(id)
+    if (not index):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f'Post with id: {id} was not found')
+    my_posts.pop(index)
+
+    return Response(status_code = status.HTTP_204_NO_CONTENT)
 
