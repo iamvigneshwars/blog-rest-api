c4abf049c9fdb120e733c27a737f91cb290d9f6f
iamvigneshwars <iamvigneshwars@gmail.com>
2022-07-17 16:44:28 +0100

    update

--- app/main.py
+++ app/main.py
@@ -1,55 +1,40 @@
-from multiprocessing import synchronize
 from fastapi import FastAPI, Response, status, HTTPException, Depends
-from fastapi.params import Body
-from pydantic import BaseModel
-from typing import Optional
-from random import randrange
-from . import models
+from . import models, schemas  
 from .database import engine, get_db
 from sqlalchemy.orm import Session
+from typing import List
 
 models.Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
 
-class Post(BaseModel):
-    title : str
-    content: str
-    published : bool = True
-    # rating : Optional[int] = None
-
-my_posts = [
-    {"title" : "TITLE 1", "content" : "This is the first post", "id" : 1},
-
-    {"title" : "TITLE 2", "content" : "I like pizza", "id" : 2}
-]
-
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"Info": "This is an blog post CRUD API that allows users to create, update, and delete a post"}
 
 # Get all the posts
-@app.get("/posts")
+@app.get("/posts", response_model = List[schemas.PostResponse])
 def root(db : Session = Depends(get_db)):
-    # return {"message": "This is the post page"}
+
     posts = db.query(models.Post).all()
-    # print(my_posts[0]["id"])
-    return {"data": posts}
+
+    return posts 
 
 # Create a new post in a database
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create(post : Post, db: Session = Depends(get_db)):
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
+def create(post : schemas.PostCreate, db: Session = Depends(get_db)):
    
     new_post = models.Post(**post.dict())
-
+    
+    # Add the new post content to the database
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
 
-    return {"data": new_post}
+    return new_post 
 
 # Get a post with specific ID
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model = schemas.PostResponse)
 def get_post(id: int, db : Session = Depends(get_db)):
 
     post = db.query(models.Post).where(models.Post.id == id).first()
@@ -57,13 +42,12 @@ def get_post(id: int, db : Session = Depends(get_db)):
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"post with id: {id} not found")
       
-    return {"post" : post}
+    return post 
 
 
 # Delete a post from the database
 @app.delete("/posts/{id}", status_code = status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db : Session = Depends(get_db)):
-    # find the index in the array that has the required ID
 
     post = db.query(models.Post).where(models.Post.id == id)
 
@@ -77,19 +61,16 @@ def delete_post(id: int, db : Session = Depends(get_db)):
 
 
 # Update a post in the database
-@app.put("/posts/{id}")
-def update_post(id : int, updated_post : Post, db : Session = Depends(get_db)):
+@app.put("/posts/{id}", response_model = schemas.PostResponse)
+def update_post(id : int, updated_post : schemas.PostCreate, db : Session = Depends(get_db)):
 
     post_query = db.query(models.Post).filter(models.Post.id == id)
-
     post = post_query.first()
 
-    # index = find_index_post(id)
     if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f'Post with id: {id} was not found')
 
-    # print(**post_in.dict())
-    post_query.update(update_post.dict())
+    post_query.update(updated_post.dict())
     db.commit()
 
-    return {'data' : post_query.first()}
+    return post_query.first()
