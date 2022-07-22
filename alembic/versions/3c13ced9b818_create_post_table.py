b84633e02da3410457a3bf73aa0b22458656087e
iamvigneshwars <iamvigneshwars@gmail.com>
2022-07-22 20:42:09 +0100

    alembic initial commit

---
+++ alembic/versions/3c13ced9b818_create_post_table.py
@@ -0,0 +1,24 @@
+"""create post table
+
+Revision ID: 3c13ced9b818
+Revises: 
+Create Date: 2022-07-22 20:41:04.093864
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3c13ced9b818'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
--- app/main.py
+++ app/main.py
@@ -2,12 +2,23 @@ from fastapi import FastAPI, APIRouter
 from . import models, utils  
 from .database import engine
 from .routers import post, user, auth
+from fastapi.middleware.cors import CORSMiddleware
 
 router = APIRouter()
 
 models.Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
